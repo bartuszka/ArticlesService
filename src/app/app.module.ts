836a0d8e03398f678baddc59a4494a8746c39c87
Bartosz Chorąży <bartek.chor@gmail.com>
2017-09-16 12:16:54 +0200

    eveything addad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MdTabsModule } from '@angular/material';
+import {MdCardModule} from '@angular/material';
+import { MainPageModule } from './main-page/main-page.module';
+import { AppRoutingModule } from './app.routing.module';
+import { LoginPageModule } from './login-page/login-page.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MainPageModule,
+    AppRoutingModule,
+    LoginPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainPageComponent } from "./main-page/main-page.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: 'main', pathMatch: 'full' },
+    { path: 'main', component: MainPageComponent },
+    { path: 'login', component: LoginPageComponent }
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule{}
